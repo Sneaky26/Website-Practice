633714c9281ed4a941f76ad6ecb7cc9b6a556cba
Sneaky26 <justinedimla000@gmial.com>
2026-01-29 19:25:21 +0800

    practice

--- JavaSwing/cafe.java
+++ JavaSwing/cafe.java
@@ -1,4 +1,5 @@
 package JavaSwing;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -7,31 +8,31 @@ public class cafe {
     public static void main(String[] args) {
         // Main Frame
         JFrame frame = new JFrame("Jolibee Cashier GUI");
-        frame.setLayout(null);
+        frame.setLayout(new GridLayout(0,3,10,10));
         
         // Side Menu
         JPanel sideMenuPanel = new JPanel();
         sideMenuPanel.setBackground(new Color(175, 175, 175));
-        sideMenuPanel.setBounds(50, 100, 400, 850);
+        sideMenuPanel.setPreferredSize(new Dimension(300,400));
         
         // Main Panel
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(new Color(175, 175, 175));
-        mainPanel.setBounds(500, 100, 925, 850);
-        mainPanel.setLayout(null); 
-        
-        // Item Box (inside mainPanel)
-        JPanel itemBox = new JPanel();
-        itemBox.setBackground(new Color(230, 230, 230));
-        itemBox.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));
-        itemBox.setBounds(50, 50, 825, 750); 
-        
+        mainPanel.setPreferredSize(new Dimension(300,400));
+        mainPanel.setLayout(new GridLayout(0,3,10,10)); 
+
         // Image Box
         String[] images = {"Burger.jpg", "Pizza.jpg", "Fries.jpg"};
         double [] price = {5.99,10.99,2.99};
         for (int i = 0; i < images.length;i++) {
-            
             String img = images[i];
+                    
+            JPanel itemPanel = new JPanel(new BorderLayout());
+            itemPanel.setPreferredSize(new Dimension(125, 220));
+
+            // lowerBox 
+            JPanel lowerBox = new JPanel(new FlowLayout());
+
             ImageIcon icon = new ImageIcon("JavaSwing/swingImages/" + img);
             Image scaledImage = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
             ImageIcon scaledIcon = new ImageIcon(scaledImage);
@@ -41,15 +42,29 @@ public class cafe {
             label.setText("<html><center>" + name + "<br>$" + price[i] + "</center></html>");
             label.setVerticalTextPosition(JLabel.BOTTOM);
             label.setHorizontalTextPosition(JLabel.CENTER);
-
+            label.setPreferredSize(new Dimension(125,175));
             label.setOpaque(true);
-            label.setBackground(Color.WHITE); 
-            label.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-            itemBox.add(label);
+            label.setBackground(Color.pink); 
+            
+
+            JButton plusButton = new JButton("+");
+            plusButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("hey you click it");
+                }
+            });
+
+            JButton minusButton = new JButton("-");
+            
+            lowerBox.add(plusButton);
+            lowerBox.add(minusButton);
+
+            itemPanel.add(label, BorderLayout.CENTER);
+            itemPanel.add(lowerBox,BorderLayout.SOUTH);
+
+            mainPanel.add(itemPanel);
         }
-        
-        mainPanel.add(itemBox); 
-        
+            
         // Receipt Panel
         JLabel receiptLabel = new JLabel("Receipt List");
         receiptLabel.setFont(new Font("Arial", Font.BOLD, 18));
@@ -65,6 +80,7 @@ public class cafe {
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        
         
     }
 }
---
+++ Jolibee2/jabilee.java
@@ -0,0 +1,9 @@
+package Jolibee2;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class jabilee {
+    public static void main(String[] args) {
+        
+    }
+}
