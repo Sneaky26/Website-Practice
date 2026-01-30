0580c3708040fbdf5912b36da3ad900a70484172
Sneaky26 <justinedimla000@gmail.com>
2026-01-30 19:04:32 +0800

    hey

--- Jolibee2/jabilee.java
+++ Jolibee2/jabilee.java
@@ -141,6 +141,7 @@ public class jabilee {
             
             mainPanel.add(itemBox);
         }
+
         frame.setLayout(new FlowLayout(FlowLayout.LEFT,75,100));
         frame.add(menuPanelWrapper);
         frame.add(mainPanelWrapper);
