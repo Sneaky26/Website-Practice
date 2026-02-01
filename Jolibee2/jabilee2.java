bf47cccf6544246a8f2ed80f731454f89ffb5ac0
Sneaky26 <justinedimla000@gmail.com>
2026-02-01 13:51:59 +0800

    feb1

--- Jolibee2/jabilee2.java
+++ Jolibee2/jabilee2.java
@@ -22,7 +22,36 @@ class TransactionDetail{
 }
 
 public class jabilee2 {
-    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel menuPanel = createPanel(300,800, Color.red);
+        JPanel mainPanel = createPanel(900,700, Color.pink);
+        mainPanel.setLayout(new FlowLayout(FlowLayout.LEFT,60,40));
+        JPanel receiptPanel = createPanel(400, 800, Color.gray);
+
+        String [] images = {"Pizza.jpg","Fries.jpg","Burger.jpg"};
+        double [] price = {9.99, 3.99, 5.99};
+        
+        for (int i = 0; i < images.length ; i++){
+            String imagePath = "Jolibee2/Images/" + images[i];
+            String name = images[i].replace(".jpg","").replace(".png","");
+
+            JPanel itemBox = createItemBox(imagePath,name,price[i],receiptPanel);
+            
+            mainPanel.add(itemBox);
+        }
+         
+        
+        frame.setLayout(new FlowLayout(FlowLayout.LEFT,75,100));
+        frame.add(menuPanel);
+        frame.add(mainPanel);
+        frame.add(receiptPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setTitle("Cashier GUI");
+        frame.setVisible(true);
+    }
+
     // Recycle Panel
     public static JPanel createPanel(int width, int height, Color color){
         JPanel panel = new JPanel();
@@ -32,11 +61,16 @@ public class jabilee2 {
         return panel;
     }
     // UPDATE RECEIPT
-    private static void updateReceiptLabel(JLabel label, int qty, String name, double price){
+    private static void updateReceiptLabel(JLabel receiptDetail,JLabel totalCost,ImageIcon icon,int qty, String name, double price){
         TransactionDetail td = new TransactionDetail(qty, name, price);
-        label.setText(td.getQuantity() + "x " + td.getName() + " $" + td.getPrice());
-        label.setVisible(true);
+        receiptDetail.setIcon(icon);
+        receiptDetail.setText(td.getQuantity() + "x " + td.getName() + " $" + td.getPrice());
+        receiptDetail.setVisible(true);
+
+        totalCost.setText("Total: $" + String.format("%.2f", td.getTotal()));
+        totalCost.setVisible(true);
     }
+    
     // CREATE ITEM BOX
     public static JPanel createItemBox(String imagePath, String name, double price, JPanel receiptPanel){
         JPanel itemBox = new JPanel(new BorderLayout());
@@ -65,23 +99,38 @@ public class jabilee2 {
         JLabel totalCost = new JLabel();
         totalCost.setForeground(Color.WHITE);
 
+        receiptPanel.add(receiptDetail);
+        receiptPanel.add(totalCost);
+
         // minus
         JButton minus = new JButton("-");
+        JButton plus = new JButton("+");
+
         minus.setBackground(Color.red);
         minus.addActionListener(e ->{
-            if (num[0] < 0){
+            if (num[0] <= 0){
                 minus.setEnabled(false);
+                receiptDetail.setVisible(false);
+                totalCost.setVisible(false);
             } else {
+                num[0]--;
+                plus.setEnabled(true);
                 quantity.setText(String.valueOf(num[0]));
+                updateReceiptLabel(receiptDetail,totalCost,scaledImages,Integer.parseInt(quantity.getText()), name, price);
             }
-
         });
-
+        
         // plus
-        JButton plus = new JButton("+");
         plus.setBackground(Color.green);
         plus.addActionListener(e -> {
-
+            if (num[0] >= 20){
+                plus.setEnabled(false);
+            } else {
+                num[0]++;
+                minus.setEnabled(true);
+                quantity.setText(String.valueOf(num[0]));
+                updateReceiptLabel(receiptDetail,totalCost,scaledImages, Integer.parseInt(quantity.getText()), name, price);
+            }
         });
 
         // Quantity Limiter
@@ -103,37 +152,6 @@ public class jabilee2 {
         itemBox.add(lowerBox,BorderLayout.SOUTH);
 
         return itemBox;
-    }
-
-    public static void main(String[] args) {
-        
-        JFrame frame = new JFrame();
-        JPanel menuPanel = createPanel(300,800, Color.red);
-        JPanel mainPanel = createPanel(900,700, Color.pink);
-        mainPanel.setLayout(new FlowLayout(FlowLayout.LEFT,60,40));
-        JPanel receiptPanel = createPanel(400, 800, Color.gray);
-
-        String [] images = {"Pizza.jpg","Fries.jpg","Burger.jpg"};
-        double [] price = {9.99, 3.99, 5.99};
-        
-        for (int i = 0; i < images.length ; i++){
-            String imagePath = "Jolibee2/Images/" + images[i];
-            String name = images[i].replace(".jpg","").replace(".png","");
-
-            JPanel itemBox = createItemBox(imagePath,name,price[i],receiptPanel);
-            
-            mainPanel.add(itemBox);
-        }
-         
-        
-        frame.setLayout(new FlowLayout(FlowLayout.LEFT,75,100));
-        frame.add(menuPanel);
-        frame.add(mainPanel);
-        frame.add(receiptPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setTitle("Cashier GUI");
-        frame.setVisible(true);
     }
 }
 
